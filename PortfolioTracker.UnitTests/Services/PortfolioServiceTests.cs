8999f1297331a9c308fb7b4e1da297cce2f7ed17
Preetham K H <blighter.soul@gmail.com>
2025-12-30 15:16:22 +1100

    WIP - PortfolioTests (Unit) - GetUserPortfoliosAsync

--- PortfolioTracker.UnitTests/Services/PortfolioServiceTests.cs
+++ PortfolioTracker.UnitTests/Services/PortfolioServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Moq;
+using PortfolioTracker.Core.Entities;
 using PortfolioTracker.Core.Interfaces.Repositories;
 using PortfolioTracker.Core.Services;
 
@@ -25,6 +27,67 @@ namespace PortfolioTracker.UnitTests.Services
             _mockPortfolioRepository = new Mock<IPortfolioRepository>();
             _mockUserRepository = new Mock<IUserRepository>();
             _portfolioService = new PortfolioService(_mockPortfolioRepository.Object, _mockUserRepository.Object, CreateMockLogger<PortfolioService>());
+        }
+
+        [Fact]
+        public async Task GetUserPortfoliosAsync_WhenPortfoliosExist_ShouldReturnAllUserPortfolios()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var portfolios = new List<Portfolio>()
+            {
+                new Portfolio
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Retirement",
+                    UserId = userId,
+                    IsDefault = true,
+                    Currency = "USD"
+                },
+                new Portfolio
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Trading",
+                    UserId = userId,
+                    IsDefault = false,
+                    Currency = "USD"
+                }
+            };
+
+            _mockPortfolioRepository
+                .Setup(repo => repo.GetByUserIdAsync(userId))
+                .ReturnsAsync(portfolios);
+
+            // Act
+            var result = (await _portfolioService.GetUserPortfoliosAsync(userId)).ToList();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(2);
+            result.First().Name.Should().Be("Retirement");
+            result.First().IsDefault.Should().BeTrue();
+
+            _mockPortfolioRepository.Verify(repo => repo.GetByUserIdAsync(userId), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetUserPortfoliosAsync_WhenNoPortfolios_ShouldReturnEmptyList()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+
+            _mockPortfolioRepository
+                .Setup(repo => repo.GetByUserIdAsync(userId))
+                .ReturnsAsync(new List<Portfolio>());
+
+            // Act
+            var result = (await _portfolioService.GetUserPortfoliosAsync(userId)).ToList();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+
+            _mockPortfolioRepository.Verify(repo => repo.GetByUserIdAsync(userId), Times.Once);
         }
     }
 }
