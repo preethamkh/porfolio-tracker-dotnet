4224eb80dd955396fd222ddf59368810cfe8d9b0
Preetham K H <blighter.soul@gmail.com>
2026-01-06 17:22:50 +1100

    #29 Update EF migration. Change TransactionType to enum from string.

---
+++ src/PortfolioTracker.Infrastructure/Migrations/20260106060422_ChangeTransactionTypeToEnum.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTransactionTypeToEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionType",
+                table: "transactions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionType",
+                table: "transactions",
+                type: "integer",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TransactionType",
+                table: "transactions",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldMaxLength: 10);
+        }
+    }
+}
